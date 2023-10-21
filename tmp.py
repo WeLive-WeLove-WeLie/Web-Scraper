88398edef7f55b4b9a8c07aacac9f3ab63e1d2ad
Wadedacoder <devm4966@gmail.com>
2023-10-21 06:16:56 +0530

    Added Product descriptions and highlights reviews now come in both lang chain and individual reviews

--- tmp.py
+++ tmp.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from langchain.document_transformers import Html2TextTransformer
 from langchain.document_loaders import AsyncHtmlLoader
 from langchain.document_transformers import BeautifulSoupTransformer
+import json
+
 #import HTML2Text
 
 
@@ -14,6 +16,10 @@ header = {
         }
 
 product = 'apple-iphone-13-pink-128-gb/p/itm6e30c6ee045d2'
+# product = 'apple-iphone-14-plus-blue-128-gb/p/itmac8385391b02b'
+
+product_details = {}
+reviews = []
 
 def get_reviews_url(soup): 
     test = soup.find_all('a', href=lambda value: value and 'product-reviews' in value)
@@ -42,26 +48,64 @@ def get_all_pages(num_pages):
         # tmp = prod_reviews_page.content
         # tmp = bs_transformer.transform_documents(tmp)
         review_pages.append(prod_reviews_url)
-    
-    return review_pages
-    
-    for i in range(0, 2):
-        with open(f'page{i+1}.txt', 'w', encoding="utf-8") as f:
-            f.write(review_pages[i].page_content)
-    
+
+    product_details['highlights'] = []
+    product_details['product_description'] = []
+    for script in soup.find_all('script', attrs={'id': 'jsonLD'}):
+        json_data  = json.loads(script.text)
+        # print(json_data)
+        product_details['rating'] = json_data[0]['aggregateRating']['ratingValue']
+        product_details['review_count'] = json_data[0]['aggregateRating']['reviewCount']
+        product_details['brand'] = json_data[0]['brand']['name']
+        product_details['name'] = json_data[0]['name']
+        product_details['image'] = json_data[0]['image']
+        product_details['price'] = json_data[0]['offers']['price']
+        break
+    for li in soup.find_all('p'):
+        # Check for any class, if any reject it
+        if li.has_attr('class'):
+            continue
+        product_details['product_description'].append(li.text.strip())
+    product_details['product_description'] = product_details['product_description'][:-13]
+    product_details['highlights'] = product_details['product_description'][2:]
+    product_details['product_description'] = product_details['product_description'][1]
     return review_pages
 
+def get_review_pages(links):
+    for link in links:
+        rev_page = []
+        page = requests.get(link, headers=header)
+        soup = BeautifulSoup(page.content, 'lxml')
+        for li in soup.find_all('div'):
+            if li.has_attr('class') and len(li['class']) == 0:
+                rev_page.append(li.text.strip())
+        rev_page = rev_page[1:-1]
+        reviews.append(rev_page)
+    return
+
 def main():
-    links = get_all_pages(2)
+    links = get_all_pages(4)[1:]
+    get_review_pages(links)
     loader = AsyncHtmlLoader(links, header)
     docs = loader.load()
     print('loaded')
     html2text = Html2TextTransformer()
     # soup_transformer = BeautifulSoupTransformer()
+    
     docs = html2text.transform_documents(docs)
     for i in range(0, len(links)):
-        with open(f'page{i+1}.txt', 'w', encoding="utf-8") as f:
+        with open(f'./product/reviews/page{i+1}.txt', 'w', encoding="utf-8") as f:
+            for line in reviews[i]:
+                f.write(line)
+                f.write('\n')
+    for i in range(0, len(links)):
+        with open(f'./product/langchain_rev/page{i+1}.txt', 'w', encoding="utf-8") as f:
             f.write(docs[i].page_content)
+            if(i == 0):
+                print(docs[i].dict())
+    # print(product_details)
+    with open('./product/product_details.json', 'w') as f:
+        json.dump(product_details, f, indent=4,sort_keys=True)
     print('done')
     return
 
